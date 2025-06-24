ac9fb8f770ddf07a7978928f50e4b84b78b7747d
rrb99 <rrobicheau@hotmail.com>
2025-06-24 11:16:58 +0100

    CodewithRemarks

--- code_to_fix.py
+++ code_to_fix.py
@@ -1,8 +1,12 @@
-def abs_value(x):
+#returns the absolute value of any number.
+def abs_value(num):
+    #if num is less than zero (negative) multiply it bu -1 to make it positive
     if x < 0:
-        return -x
+        return -num
+    # if num already postice stays the same
     else:
-        return x
+        return num
     
-abs_value(-3)
+#works with decimals and integers (example using -66.7 below)
+print(abs_value(-66.7))
     
